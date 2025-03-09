c205355bdddce0953e115f7d57697e33f7b91411
Alexander Gutierrez <JohnAlexander.GutierrezGaviria@outlook.com>
2025-03-08 20:18:35 -0500

    Initial commit - Set up Spring Boot project

---
+++ src/main/java/com/codeninjas/session_tracker/CnSessionTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.codeninjas.session_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnSessionTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnSessionTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeninjas/session_tracker/CnSessionTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeninjas.session_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnSessionTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
