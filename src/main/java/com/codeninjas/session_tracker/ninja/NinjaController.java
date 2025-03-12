4acb249f3cda9fd071b6745e939e6a9444cb3300
Alexander Gutierrez <JohnAlexander.GutierrezGaviria@outlook.com>
2025-03-11 21:29:39 -0400

    Add methods annotated with @ModelAttribute to simulate belts, levels, tasks, and senseis list fetch from DB and used in list.html template

--- src/main/java/com/codeninjas/session_tracker/ninja/NinjaController.java
+++ src/main/java/com/codeninjas/session_tracker/ninja/NinjaController.java
@@ -4,27 +4,35 @@ import com.codeninjas.session_tracker.belt.Belt;
 import com.codeninjas.session_tracker.level.Level;
 import com.codeninjas.session_tracker.sensei.Sensei;
 import com.codeninjas.session_tracker.task.Task;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
+import java.sql.Date;
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("ninja")
 public class NinjaController {
 
     @ModelAttribute(name = "ninjas")
-    public List<Ninja> ninjas() {
+    public List<Ninja> ninjas(Model model) {
+        model.addAttribute("ninja", new Ninja());
+        long currentTime = System.currentTimeMillis();
+        log.info("current time {}", currentTime);
         return Arrays.asList(
                 new Ninja("Alexander",
                         "Gutierrez",
-                        new Belt(1, "Orange"),
-                        new Level(1, 1),
-                        new Task(1, "Build 1"),
+                        new Belt(3, "Orange"),
+                        new Level(2, 2),
+                        new Task(2, "Build 2"),
+                        new Date(currentTime),
                         new Sensei(1, "John", "Gaviria")
                 )
         );
@@ -40,9 +48,43 @@ public class NinjaController {
         );
     }
 
+    @ModelAttribute(name = "levels")
+    public List<Level> levels() {
+        return Arrays.asList(
+                new Level(1, 1),
+                new Level(2, 2),
+                new Level(3, 3),
+                new Level(4, 4)
+        );
+    }
+
+    @ModelAttribute(name = "tasks")
+    public List<Task> tasks() {
+        return Arrays.asList(
+                new Task(1, "Build 1"),
+                new Task(2, "Build 2"),
+                new Task(3, "Build 3"),
+                new Task(4, "Quest")
+        );
+    }
+
+    @ModelAttribute(name = "senseis")
+    public List<Sensei> senseis() {
+        return Arrays.asList(
+                new Sensei(1, "Alexander", "Gutierrez"),
+                new Sensei(2, "John", "Gaviria")
+        );
+    }
+
     @GetMapping("/list")
     public String getNinjaList() {
         return "ninja/list";
     }
 
+    // TODO converters need to be implemented to convert String values from the form in list.html to Belt, Task, Level...
+    @PostMapping("/list")
+    public String saveNinja(Ninja ninja) {
+        log.info("Ninja {}", ninja);
+        return "redirect: /list";
+    }
 }
